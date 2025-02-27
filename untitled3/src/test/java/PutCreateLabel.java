6c7b6f495d89f610f06d01802b912fd75da03477
Kristina Lyutova <k.lyutova@mail.astondevs.ru>
2025-02-27 12:45:17 +0300

    задание 1

--- untitled3/src/test/java/PutCreateLabel.java
+++ untitled3/src/test/java/PutCreateLabel.java
@@ -2,19 +2,35 @@ import io.restassured.response.ValidatableResponse;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PutCreateLabel {
 
     public static void main(String[] args) {
-        String token = getToken();
-        infoOfLabel(token);
+        Properties properties = loadProperties();
+        String baseUrl = properties.getProperty("base_url");
+        String apiKey = properties.getProperty("api_key");
+
+        String token = getToken(baseUrl);
+        infoOfLabel(token, baseUrl, apiKey);
     }
 
+    public static Properties loadProperties() {
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("src/test/resources/application.properties")) {
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
 
-    public static String getToken() {
+    public static String getToken(String baseUrl) {
         String requestBody = "{\"username\": \"Kris\", \"password\": \"172839qQ!\"}";
-
         return given()
-                .baseUri("https://try.vikunja.io")
+                .baseUri(baseUrl)
                 .header("Content-Type", "application/json")
                 .body(requestBody)
                 .when()
@@ -27,8 +43,7 @@ public class PutCreateLabel {
                 .getString("token");
     }
 
-
-    public static ValidatableResponse infoOfLabel(String token) {
+    public static ValidatableResponse infoOfLabel(String token, String baseUrl, String apiKey) {
         String information =
                 "{ \"email\": \"Kristina1@mail.ru\", " +
                         "\"name\": \"Kristina\"," +
@@ -37,15 +52,15 @@ public class PutCreateLabel {
                         "\"hex_color\": \"#FFA500\"}";
 
         return given()
-                .baseUri("https://try.vikunja.io")
+                .baseUri(baseUrl)
                 .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer " + token)
+                .header("Authorization", "Bearer " + token) // Используем полученный токен
                 .body(information)
                 .when()
                 .put("/api/v1/labels")
                 .then()
                 .log().all()
-                .statusCode(200)
+                .statusCode(201)
                 .body("email", equalTo("Kristina1@mail.ru"))
                 .body("name", equalTo("Kristina"))
                 .body("username", equalTo("misha"))
